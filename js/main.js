999ea18f9192086e76d8a7e6b5346bd38c5303cf
Fee <h_fee@tss-hr420-dm17.fc.ca>
2017-09-20 16:10:57 -0400

    finishing feature branch

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
-//new way to do js app development
-//everything is wrapped in an object
-var myApp = {};
+(function () {
+    var theImages = document.querySelectorAll('.image-holder'),
+          theHeader = document.querySelectorAll('.header'),
+          theSubhead = document.querySelectorAll('.main-copy h2'),
+          theSeasonText = document.querySelectorAll('.main-copy p'),
+          appliedClass;
+
+          function changeElements() {
+            // load dynamic content here
+            debugger;
+          }
+
+          theImages.forEach(function (element, index) {
+            //loop through and do stuff to each element at the top of the page
+            element.addEventListener('click', changeElements, false);
+          });
+})();
--- script/main.js
+++ script/main.js
@@ -1,1 +1,49 @@
 
+(function () {
+    var theImages = document.querySelectorAll('.image-holder'),
+          theHeader = document.querySelector('.heading'),
+          theSubhead = document.querySelector('.main-copy h2'),
+          theSeasonText = document.querySelector('.main-copy p'),
+          appliedClass;
+
+          function changeElements() {
+            // load dynamic content here
+            //debugger;
+
+            let subImages = document.querySelector('.subImagesContainer');
+            let objectIndex = dynamicContent[this.id];
+
+            while (subImages.firstChild) {
+              subImages.removeChild(subImages.firstChild);
+            }
+
+            objectIndex.images.forEach(function(element, index){
+                let newSubImg = document.createElement('img');
+
+                newSubImg.classList.add('thumb');
+                newSubImg.src = "images/" + objectIndex.images[index];
+                subImages.appendChild(newSubImg);
+
+            });
+
+            theSubhead.classList.remove(appliedClass);
+            theHeader.classList.remove(appliedClass);
+
+            theSubhead.classList.add(this.id);
+            theHeader.classList.add(this.id);
+
+            theSubhead.firstChild.nodeValue = objectIndex.headline;
+            theSeasonText.firstChild.nodeValue = objectIndex.text;
+          }
+
+          theImages.forEach(function (element, index) {
+            //loop through and do stuff to each element at the top of the page
+            element.addEventListener('click', changeElements, false);
+          });
+
+          //init app
+
+          theSubhead.firstChild.nodeValue = dynamicContent['spring'].headline;
+          theSeasonText.firstChild.nodeValue = dynamicContent['spring'].text;
+          theHeader.classList.add('spring');
+})();
